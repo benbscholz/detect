012844e616b22aedadee5bb53e6964c9078d1d07
Ben Brooks Scholz <bbscholz@gmail.com>
2011-09-25 19:38:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+detect.js
+=======================
+browser & os detection
+
+# Usage
+
+Add detect.js to your page:
+
+    <script src="detect.js"></script>
+
+Retrieve the browser, version, & os:
+    
+    var ui = window.ui;
+    console.log( ui.browser );
+    console.log( ui.version );
+    console.log( ui.os );
+
+# Browsers Detected
+    
+    Internet Explorer 6, 7, 8, 9, Mobile
+    Firefox 3.6, 4, 5, 6, 7, Mobile
+    Safari 3, 4, 5, Mobile
+    Opera 9, 10, 11, Mini, Mobile 
+    Chrome
+
+# Operating Systems Detected
+
+    Mac OS X
+    Windows
+    Linux
+    Android
+    iOS
---
+++ src/detect.js
@@ -0,0 +1,100 @@
+// detect.js - browser & os detection
+// 2011 (c) Ben Brooks Scholz. MIT Licensed.
+
+;(function (window) {
+
+    var browser,
+        os,
+        version,
+        ua = window.navigator.userAgent,
+        platform = window.navigator.platform;
+
+    if ( /MSIE/.test(ua) ) {
+        
+        browser = 'Internet Explorer';
+        
+        if ( /IEMobile/.test(ua) ) {
+            browser += ' Mobile';
+        }
+        
+        version = /MSIE \d+[.]\d+/.exec(ua)[0].split(' ')[1];
+        
+    } else if ( /Chrome/.test(ua) ) {
+        
+        browser = 'Chrome';
+        version = /Chrome\/[\d\.]+/.exec(ua)[0].split('/')[1];
+        
+    } else if ( /Opera/.test(ua) ) {
+        
+        browser = 'Opera';
+        
+        if ( /mini/.test(ua) ) {
+            browser += ' Mini';
+        } else if ( /Mobile/.test(ua) ) {
+            browser += ' Mobile';
+        } 
+        
+    } else if ( /Android/.test(ua) ) {
+        
+        browser = 'Android Webkit Browser';
+        mobile = true;
+        os = /Android\s[\.\d]+/.exec(ua);
+        
+    } else if ( /Firefox/.test(ua) ) {
+        
+        browser = 'Firefox';
+        
+        if ( /Fennec/.test(ua) ) {
+            browser += ' Mobile';
+        }
+        version = /Firefox\/[\.\d]+/.exec(ua)[0].split('/')[1];
+        
+    } else if ( /Safari/.test(ua) ) {
+        
+        browser = 'Safari';
+        
+        if ( (/iPhone/.test(ua)) || (/iPad/.test(ua)) || (/iPod/.test(ua)) ) {
+            os = 'iOS';
+        }
+        
+    }
+
+    if ( !version ) {
+        
+         version = /Version\/[\.\d]+/.exec(ua);
+         
+         if (version) {
+             version = version[0].split('/')[1];
+         } else {
+             version = /Opera\/[\.\d]+/.exec(ua)[0].split('/')[1]
+         }
+         
+    }
+    
+    if ( platform === 'MacIntel' || platform === 'MacPPC' ) {
+        
+        os = 'Mac OS X ' + /10[\.\_\d]+/.exec(ua)[0];
+        if ( /[\_]/.test(os) ) {
+            os = os.split('_').join('.');
+        }
+        
+    } else if ( platform === 'Win32' ) {
+        os = 'Windows 32 bit';
+    } else if ( platform == 'Win64' ) {
+        os = 'Windows 64 bit';
+    } else if ( !os && /Linux/.test(platform) ) {
+        os = 'Linux';
+    } else if ( !os && /Windows/.test(ua) ) {
+        os = 'Windows';
+    }
+
+    window.ui = {
+        browser : browser,
+        version : version,
+        os : os
+    };        
+}(this));
+
+
+
+
